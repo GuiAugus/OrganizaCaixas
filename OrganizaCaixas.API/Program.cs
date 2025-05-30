9923c646fc85c15ea181a87f2995d55f71dd23b8
Gui <augusto.g@outlook.com.br>
2025-05-29 23:33:37 -0300

    chore: criacao do docker

--- OrganizaCaixas.API/Program.cs
+++ OrganizaCaixas.API/Program.cs
@@ -23,13 +23,11 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
